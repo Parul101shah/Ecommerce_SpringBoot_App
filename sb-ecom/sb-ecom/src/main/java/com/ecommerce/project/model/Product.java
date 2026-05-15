2d8b3de6338d49404a9000feb974c562a5c35093
Parul101shah <97349441+Parul101shah@users.noreply.github.com>
2026-05-16 00:09:29 +0530

    Implemented Validations and Pagination in Product

--- sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
+++ sb-ecom/sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
@@ -1,15 +1,22 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
+    @NotBlank
+    @Size(min = 3, message = "Product name must contain atleast 3 characters")
     private String productName;
-    private String description;
     private String image;
+
+    @NotBlank
+    @Size(min = 6, message = "Product description must contain atleast 6 characters")
+    private String description;
     private Integer quantity;
     private double price;//100
     private double discount;//25
